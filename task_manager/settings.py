0a0727bf5bc8a73721c61cdd1f5b801239158f83
ccaperna <caperna30@hotmail.com>
2024-04-03 00:34:19 +0200

    fix: add site url to allowed host

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-u7dia-8rv)a$nz=j1x4g*#ht4o^vk0eb$$$=vuseh3yk_u0n4(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ccaperna.pythonanywhere.com/"]
 
 
 # Application definition
@@ -121,7 +121,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
