c41f71888874f81964a8ddea54d423274691ae64
ccaperna <caperna30@hotmail.com>
2024-04-03 13:24:59 +0200

    fix: update settings for dev env

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = "django-insecure-u7dia-8rv)a$nz=j1x4g*#ht4o^vk0eb$$$=vuseh3yk_u0n4("
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ccaperna.pythonanywhere.com"]
 
